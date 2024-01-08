7ab93ce9d072fedf97885f512afb7be45b13b1c4
Diego VillatoroMx <107954399+diegovillatoromx@users.noreply.github.com>
2024-01-08 15:29:22 -0600

    Update create_resources.sh

--- ETL-Pipeline-weather/infrastructure/create_resources.sh
+++ ETL-Pipeline-weather/infrastructure/create_resources.sh
@@ -174,3 +174,68 @@ else
 
     echo "Lambda function $FUNCTION_NAME created successfully."
 fi
+
+
+
+echo "Starting the creation of the Lambda function for sending weather updates..."
+
+# Load the configuration from lambda_config.json
+CONFIG_FILE="lambda_config.json"
+
+# Load the Lambda function configuration from the .json file
+LAMBDA_DIR=$(jq -r '.LambdaDir' < "$CONFIG_FILE")
+ZIP_FILE=$(jq -r '.ZipFile' < "$CONFIG_FILE")
+FUNCTION_NAME=$(jq -r '.FunctionName' < "$CONFIG_FILE")
+# ... (rest of the configuration)
+
+ROOT_DIR=$(pwd) # Save the current directory
+
+echo "Preparing the deployment package..."
+
+# Changing to the Lambda function directory and creating the ZIP file
+cd "$LAMBDA_DIR"
+if [ ! -f "../${ZIP_FILE}" ]; then
+    echo "Creating ZIP file: ${ZIP_FILE}..."
+    zip -r9 "../${ZIP_FILE}" "send_message.py"
+    echo "Created ZIP file: ${ZIP_FILE}"
+else
+    echo "ZIP file ${ZIP_FILE} already exists."
+fi
+
+# Returning to the original directory
+cd "$ROOT_DIR"
+
+# Check if the Lambda function already exists
+if aws lambda get-function --function-name "$FUNCTION_NAME" 2>/dev/null; then
+    echo "The Lambda function $FUNCTION_NAME already exists. Moving to update."
+    # Update the function code if it already exists (you might want to update other configurations too)
+    aws lambda update-function-code \
+        --function-name "$FUNCTION_NAME" \
+        --zip-file "fileb://${ZIP_FILE}"
+    echo "Lambda function $FUNCTION_NAME updated successfully."
+else
+    # If function does not exist, create it with the specified configurations
+    ROLE=$(jq -r '.Role' < "$CONFIG_FILE")
+    HANDLER=$(jq -r '.Handler' < "$CONFIG_FILE")
+    RUNTIME=$(jq -r '.Runtime' < "$CONFIG_FILE")
+    DESCRIPTION=$(jq -r '.Description' < "$CONFIG_FILE")
+    TIMEOUT=$(jq -r '.Timeout' < "$CONFIG_FILE")
+    MEMORY_SIZE=$(jq -r '.MemorySize' < "$CONFIG_FILE")
+    ENVIRONMENT=$(jq -r '.Environment' < "$CONFIG_FILE" | jq -c .)
+    LAYERS=$(jq -r '.Layers' < "$CONFIG_FILE")
+
+    echo "Creating new Lambda function $FUNCTION_NAME..."
+    aws lambda create-function \
+        --function-name "$FUNCTION_NAME" \
+        --runtime "$RUNTIME" \
+        --role "$ROLE" \
+        --handler "$HANDLER" \
+        --description "$DESCRIPTION" \
+        --timeout "$TIMEOUT" \
+        --memory-size "$MEMORY_SIZE" \
+        --environment "$ENVIRONMENT" \
+        --layers "$LAYERS" \
+        --zip-file "fileb://${ZIP_FILE}"
+
+    echo "Lambda function $FUNCTION_NAME created successfully."
+fi
